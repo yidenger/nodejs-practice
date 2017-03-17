dd93d2e22710c48b992b76d01da8af86112be3df
seth <liyidong@yeezon.com>
2017-03-17 20:04:19 +0800

    feat

---
+++ basic_module/globals/console/dir.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const obj = {
+    name: 'tj',
+    age: 38,
+    weight: 70
+};
+
+console.log(obj);
+console.dir(obj, {
+    colors: true,
+    showHidden: true
+});
---
+++ network/udp_client.js
@@ -0,0 +1,10 @@
+'use strict';
+const dgram = require('dgram');
+
+const message = new Buffer("深入浅出Node.js");
+const client = dgram.createSocket("udp4");
+
+client.send(message, 0, message.length, 41234, "localhost", function (err, bytes) {
+    console.log('message send');
+    client.close();
+});
