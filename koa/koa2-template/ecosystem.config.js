45397bdbd7836040741e76776463a4075c1aacbd
seth <liyidong@yeezon.com>
2017-09-22 11:44:02 +0800

    koa

--- koa/koa2-template/ecosystem.config.js
+++ koa/koa2-template/ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
     // First application
     {
       name: 'koa',
       script: './bin/www.js',
       watch: true,
-      ignore_watch: ["node_modules", "public"],
+      ignore_watch: ['node_modules', 'public'],
       env: {
         NODE_ENV: 'development',
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production',
-      }
+      },
     },
   ],
 
@@ -23,25 +23,25 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
+    dev: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/development',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env: {
+        NODE_ENV: 'dev',
+      },
+    },
+  },
 };
